dd20247a03751f5031389ad14f2c10544ab04a71
Nataliya Kim <magistr1982@gmail.com>
2023-11-02 13:21:19 +0200

    Add RightSidebar and clerk/themes

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,19 @@
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
-  )
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-light-1 text-heading4-medium">
+          Suggested Communities
+        </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-light-1 text-heading4-medium">
+          Suggested Users
+        </h3>
+      </div>
+    </section>
+  );
 }
 
 export default RightSidebar
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { SignedIn, SignOutButton, OrganizationSwitcher } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
 const Topbar = () => {
   return (
@@ -26,7 +27,14 @@ const Topbar = () => {
             </SignOutButton>
           </SignedIn>
         </div>
-        <OrganizationSwitcher />
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: 'py-2 px-4',
+            },
+          }}
+        />
       </div>
     </nav>
   );
