de5f2ee227bfdc6ddae757fa081a63c104fb2518
Nataliya Kim <magistr1982@gmail.com>
2023-11-06 15:39:26 +0200

    Add PostThread form

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -41,9 +41,36 @@ const PostThread = ({ userId }: { userId: string }) => {
       accountId: userId,
     },
   });
-    return (
-        <h2 className='head-text'> Post Thread Form</h2>
-    );
+    const onSubmit = async () => {
+    //   await createThread()
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start gap-10 mt-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1 ">
+                <Textarea rows={15} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default PostThread;
