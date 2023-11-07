450b517c10ebec5bfdb4b528a7dcba0355f9d8e2
Nataliya Kim <magistr1982@gmail.com>
2023-11-07 14:52:27 +0200

    Add ThreadCard and fetchThreads

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -34,3 +34,31 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+  //  Calculate the numbers of threads to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+  // fetch the threads that have no parents (top-level threads)
+  const threadsQuery = Thread.find({
+    parentId: { $in: [null, undefined] },
+  })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image',
+      },
+    });
+  const totalThreadsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+  const threads = await threadsQuery.exec();
+  const isNext = totalThreadsCount > skipAmount + threads.length;
+  return { threads, isNext };
+}
