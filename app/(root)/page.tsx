76a1e2b52be228e961573becd7f2372594f10dbf
Nataliya Kim <magistr1982@gmail.com>
2023-11-07 15:22:54 +0200

    Add icons to ThreadCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
               <ThreadCard
                 key={thread._id}
                 id={thread._id}
-                currentUserId={user?.id}
+                currentUserId={user?.id || ''}
                 parentId={thread.parentId}
                 content={thread.text}
                 author={thread.author}
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 interface Props {
   id: string;
   currentUserId: string;
@@ -32,7 +35,68 @@ const ThreadCard = ({
   createdAt,
   comments,
 }: Props) => {
-  return <div>ThreadCard</div>;
+  return (
+    <article className="flex flex-col w-full rounded-xl bg-dark-2 p-7">
+      <div className="flex items-start justify-between">
+        <div className="flex flex-1 w-full flex-row gap-4">
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative w-11 h-11">
+              <Image
+                src={author.image}
+                alt="Profile image"
+                width={44}
+                height={44}
+                className="cursor-pointer rounded-full"
+              />
+            </Link>
+            <div className="thread-card_bar" />
+          </div>
+          <div className="flex flex-col w-full">
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="text-base-semibold cursor-pointer text-light-1">
+                {author.name}
+              </h4>
+            </Link>
+            <p className="mt-2 text-small-regular text-light-2">{content}</p>
+            <div className="mt-5 flex flex-col gap-3">
+              <div className="flex gap-3.5">
+                <Image
+                  src="/assets/heart-gray.svg"
+                  alt="heart icon"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Link href={`/thread/${id}`}>
+                  <Image
+                    src="/assets/reply.svg"
+                    alt="reply icon"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer object-contain"
+                  />
+                </Link>
+                <Image
+                  src="/assets/repost.svg"
+                  alt="repost icon"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Image
+                  src="/assets/share.svg"
+                  alt="share icon"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default ThreadCard;
