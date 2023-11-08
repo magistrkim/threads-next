1ef4e84582ada957bb513cb4a062688ac23642f0
Nataliya Kim <magistr1982@gmail.com>
2023-11-08 11:08:08 +0200

    Create  fetchThreadById and ThreadPage

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,6 +34,7 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
   return (
     <article className="flex flex-col w-full rounded-xl bg-dark-2 p-7">
@@ -91,6 +92,11 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className='mt-1 text-subtle-medium text-gray-1'>{comments.length} replies</p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
