bb310f46f7645d7e8cf8cac104edb6758ae80999
Nataliya Kim <magistr1982@gmail.com>
2023-11-10 12:05:27 +0200

    Add fetchUsers to the user.actions

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,11 +1,24 @@
-
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { fetchUser } from '@/lib/actions/user.actions';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { profileTabs } from '@/constants';
+import Image from 'next/image';
+import ThreadsTab from '@/components/shared/ThreadsTab';
 
 const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect('/onboarding');
+    
+    // Fetch users
+    
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
     </section>
   );
-}
+};
 
-export default Page
+export default Page;
