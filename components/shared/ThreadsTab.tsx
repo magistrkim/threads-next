9a25d1d97f23f79f8a05be073cb679e19b929884
Nataliya Kim <magistr1982@gmail.com>
2023-11-10 08:48:00 +0200

    Add fetchUserPosts to the backend

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,9 +1,13 @@
-
-
-const ThreadsTab = () => {
-  return (
-    <div>ThreadsTab</div>
-  )
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
 }
 
-export default ThreadsTab
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+    // TODO: Fetch profile threads
+
+  return <section>ThreadsTab</section>;
+};
+
+export default ThreadsTab;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { revalidatePath } from 'next/cache';
 import User from '../models/user.model';
+import Thread from '../models/thread.model';
 import { connectToDB } from '../mongoose';
 
 interface Params {
@@ -51,5 +52,30 @@ export async function fetchUser(userId: string) {
     // });
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by user with his userId
+    // TODO: Populate the community
+    const threads = await User.findOne({ id: userId }).populate({
+      path: 'threads',
+      model: Thread,
+      populate: {
+        path: 'children',
+        model: Thread,
+        populate: {
+          path: 'author',
+          model: User,
+          select: 'name image id',
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`);
   }
 }
