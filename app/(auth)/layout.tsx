9c4b8a5aaa33f4371ceba100b80a8d697b250577
Nataliya Kim <magistr1982@gmail.com>
2023-11-11 12:49:28 +0200

    Add activity page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Inter } from 'next/font/google';
-import "../globals.css";
+import '../globals.css';
 
 export const metadata = {
   title: 'Threads',
@@ -9,14 +9,18 @@ export const metadata = {
 };
 const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -121,3 +121,27 @@ export async function fetchUsers({
     throw new Error(`Failed to fetch users: ${error.message}`);
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+    // find all threads created by this user
+    const userThreads = await Thread.find({ author: userId });
+
+    // collect all child thread ids (or replies)
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id',
+    });
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message}`);
+  }
+}
