1dfb7b12825f7937c6cddacf733094ed6e9c4747
Nataliya Kim <magistr1982@gmail.com>
2023-11-14 13:38:30 +0200

    Add Searchbar

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,14 +1,17 @@
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
-import UserCard from '@/components/cards/UserCard';
-import ProfileHeader from '@/components/shared/ProfileHeader';
-import { profileTabs } from '@/constants';
-import Image from 'next/image';
+
+import { fetchUser } from '@/lib/actions/user.actions';
 import { fetchCommunities } from '@/lib/actions/community.actions';
+
+import Searchbar from '@/components/shared/Searchbar';
 import CommunityCard from '@/components/cards/CommunityCard';
 
-const Page = async () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
   const user = await currentUser();
   if (!user) return null;
   const userInfo = await fetchUser(user.id);
@@ -16,16 +19,18 @@ const Page = async () => {
 
   // Fetch communities
   const result = await fetchCommunities({
-    searchString: '',
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
   return (
-    <section>
+    <>
       <h1 className="head-text mb-10">Communities</h1>
-      {/* { Search Bar} */}
-      <div className="mt-14 flex flex-col gap-9">
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
+      <section className="mt-9 flex flex-wrap gap-4">
         {result.communities.length === 0 ? (
           <p className="no-result">No communities found</p>
         ) : (
@@ -43,8 +48,8 @@ const Page = async () => {
             ))}
           </>
         )}
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -2,11 +2,13 @@ import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
 import UserCard from '@/components/cards/UserCard';
-import ProfileHeader from '@/components/shared/ProfileHeader';
-import { profileTabs } from '@/constants';
-import Image from 'next/image';
+import Searchbar from '@/components/shared/Searchbar';
 
-const Page = async () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
   const user = await currentUser();
   if (!user) return null;
   const userInfo = await fetchUser(user.id);
@@ -15,15 +17,15 @@ const Page = async () => {
   // Fetch users
   const result = await fetchUsers({
     userId: user.id,
-    searchString: '',
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
-      {/* { Search Bar} */}
+      <Searchbar routeType="search" />
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
           <p className="no-result">No users found</p>
